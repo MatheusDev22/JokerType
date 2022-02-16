46aa3dea0079041f1ece537163251fa2e31fde60
MatheusDev22 <matheusbsb95@gmail.com>
2022-02-16 16:23:04 -0300

    cannot upcast from List<Integer> to List<object>

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,20 @@
 package application;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Program {
 	
 	public static void main (String[] args) {
-		List<?> myObjs = new ArrayList<Object>();
-		List<Integer> myNumber = new ArrayList<Integer>();
-		myObjs = myNumber;
+		List<Integer> myInts = Arrays.asList(5, 2, 10);
+		printList(myInts);
+	}
+
+	public static void printList(List<?> list) {
+		for(Object obj : list) {
+			System.out.println(obj);
+		}
 		
-		Object obj;
-		Integer x = 10;
 	}
 
 }
