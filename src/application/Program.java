19b7aae904f3d9b1dac0c56865d4ab5a0bdca7f4
MatheusDev22 <matheusbsb95@gmail.com>
2022-02-16 16:14:27 -0300

    List<?>

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,17 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
+	
+	public static void main (String[] args) {
+		List<?> myObjs = new ArrayList<Object>();
+		List<Integer> myNumber = new ArrayList<Integer>();
+		myObjs = myNumber;
+		
+		Object obj;
+		Integer x = 10;
+	}
 
 }
