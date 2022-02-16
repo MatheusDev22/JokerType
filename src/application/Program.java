b30500cf8871dbc6799d4b5825d59e1a412224af
MatheusDev22 <matheusbsb95@gmail.com>
2022-02-16 16:28:17 -0300

    can i use printList for any kind of list List<?>

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,6 +8,9 @@ public class Program {
 	public static void main (String[] args) {
 		List<Integer> myInts = Arrays.asList(5, 2, 10);
 		printList(myInts);
+		
+		List<String> myStr = Arrays.asList("Maria", "Alex", "Bob");
+		printList(myStr);
 	}
 
 	public static void printList(List<?> list) {
